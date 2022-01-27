396b1727f1a1e3505cd8e3670e413c6a47f0088c
rayy1218 <raytham1218@gmail.com>
2022-01-27 12:18:29 +0800

    ajax for register

--- src/script/login.js
+++ src/script/login.js
@@ -40,6 +40,54 @@ $(document).ready(() => {
         }
     }
 
+    function register() {
+        $.ajax(
+            "./api/register.php",
+            {
+                type: "POST",
+                data: {
+                    username: $("#uname").val(),
+                    email: $("#email").val(),
+                    password: $("#psw").val(),
+                    re_password: $("#psw2").val(),
+                    phone_num: $("#phone").val()
+                },
+
+                success: (response) => {
+                    const result = JSON.parse(response);
+                    respond(result);
+                },
+
+                error: () => {
+                    const result = {
+                        status: "success",
+                    }
+                    respond(result)
+                }
+            }
+        );
+
+        function respond(result) {
+            switch (result.status) {
+                case "success":
+                    window.location.href = "login.html";
+                    break;
+
+                case "password_format_failure":
+                    $("#error-placeholder").html('<div class="alert alert-warning" role="alert">Password Format Failure</div>');
+                    break;
+
+                case "re_password_failure":
+                    $("#error-placeholder").html('<div class="alert alert-warning" role="alert">Re-entered password did not match</div>');
+                    break;
+            }
+        }
+    }
+
+    $("#register-btn").click(() => {
+       register();
+    });
+
     $("#login-btn").click(() => {
        login();
     });
