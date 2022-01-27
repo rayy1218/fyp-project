b964c2ccb55b76f031f8d9cb8167822663b11fb8
rayy1218 <raytham1218@gmail.com>
2022-01-27 14:14:45 +0800

    header and prototype use navi modal

--- src/sql/dummy_data.sql
+++ src/sql/dummy_data.sql
@@ -1,17 +1,15 @@
 DELETE FROM Member WHERE NOT member_id = 1;
-INSERT INTO Member (member_first_name, member_last_name, member_email, member_password, member_phone, member_birthday)
+INSERT INTO Member (member_username, member_email, member_password, member_phone, member_birthday)
 VALUES
 (
-    'Second',
-    'User',
+    'Second User'
     'second_user@gmail.com',
     '1234',
     '012-3456789',
     '2000-01-01'
 ),
 (
-    'Third',
-    'User',
+    'Third User',
     'third_user@gmail.com',
     '1234',
     '012-9876543',
