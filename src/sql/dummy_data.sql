661581eb89414e1e96c0b3292ef33253027d6e4d
ChuaSiongYong <68434748+ChuaSiongYong@users.noreply.github.com>
2022-01-27 12:35:30 +0800

    Update dummy_data.sql

--- src/sql/dummy_data.sql
+++ src/sql/dummy_data.sql
@@ -18,3 +18,26 @@ VALUES
     '2001-01-01'
 );
 
+INSERT INTO Movies (movie_title, movie_duration, movie_description, movie_thumbnail, movie_genre, movie_language, movie_censorship_rating, movie_rating)
+VALUES
+(
+    'bla1',
+    '1234',
+    'bla1',
+    'bla1',
+    'bla1',
+    'bla1',
+    'bla1',
+    'bla1',
+),
+(
+    'bla2',
+    '12345',
+    'bla2',
+    'bla2',
+    'bla2',
+    'bla2',
+    'bla2',
+    'bla2',
+);
+
