5908ae79d659c48c3e467627b3a539215e73e17c
kai054631 <88898686+kai054631@users.noreply.github.com>
2022-03-06 20:27:13 +0800

    Update homepage.php

--- src/api/homepage.php
+++ src/api/homepage.php
@@ -1,2 +1,29 @@
 <?php
 header("HTTP/1.1 501 Not Implemented");
+<?php
+include "connection.php";
+include "crud.php";
+
+$action = 0;
+if (isset($_POST["action"])) {
+    $action = $_POST["action"];
+}
+else if (isset($_GET["action"])) {
+    $action = $_GET["action"];
+}
+
+switch ($action) {
+    case "promoted-movie":
+       //TBD
+        break;
+	case "recent-movie":
+       //return array[9] of object["movie_thumbnail, movie_id, movie_title"] sort by movie_added_time
+        
+		
+		break;
+	case "movie-today":
+		//return array[9] of object["movie_thumbnail, movie_id, movie_title"] from scheduled movie where movie_showing_date = today
+       
+        break;
+    
+}
