54fa98d153c43a918371059cf0ae579aa33d3a19
kai054631 <88898686+kai054631@users.noreply.github.com>
2022-03-09 14:29:38 +0800

    Main page and dashboard php updated

--- src/api/homepage.php
+++ src/api/homepage.php
@@ -1,2 +1,22 @@
 <?php
 header("HTTP/1.1 501 Not Implemented");
+$action = 0;
+if (isset($_POST["action"])) {
+    $action = $_POST["action"];
+}
+else if (isset($_GET["action"])) {
+    $action = $_GET["action"];
+}
+switch ($action) {
+case "promoted-movie":
+    //TBD
+    break;
+case "recent-movie":
+    SELECT movie_thumbnail,movie_id,movie_title FROM Member
+	ORDER BY movie_added_time;
+    break;
+case "movie-today":
+    SELECT movie_thumbnail, movie_id, movie_title FROM Member
+	ORDER BY  movie_showing_date = today;
+    break;
+}
