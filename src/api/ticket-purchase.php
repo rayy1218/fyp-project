a011633eae28c1d9168e294ef6a587d56428bb9f
Ray Tham <65101833+rayy1218@users.noreply.github.com>
2022-03-18 17:07:34 +0800

    Update ticket-purchase.php

--- src/api/ticket-purchase.php
+++ src/api/ticket-purchase.php
@@ -2,19 +2,22 @@
 include "connection.php";
 include "crud.php";
 
+date_default_timezone_set('Asia/Kuala_Lumpur');
+$date = date("y-m-d");
+$time = date("G:i:s");
+
 switch ($_GET["action"]) {
   case "get-cinema-list":
-    $statement = mysqli_prepare($conn,
-            "
-                SELECT 
-                    Cinema.id, 
-                FROM Scheduled_Movie
-                JOIN Theater ON Scheduled_Movie.theater_id = Theater.theater_id
-                JOIN Cinema ON Theater.cinema_id = Cinema.cinema_id
-                WHERE Scheduled_Movie.movie_id = 1 AND
-                scheduled_movie_showing_date > '2022-03-08' OR
-                scheduled_movie_showing_date = '2022-03-08' AND scheduled_movie_start_time > '14:30:00';
-    );
+    $statement = mysqli_prepare($conn, "
+        SELECT 
+            Cinema.id, 
+        FROM Scheduled_Movie
+        JOIN Theater ON Scheduled_Movie.theater_id = Theater.theater_id
+        JOIN Cinema ON Theater.cinema_id = Cinema.cinema_id
+        WHERE Scheduled_Movie.movie_id = ? AND
+        scheduled_movie_showing_date > ? OR
+        scheduled_movie_showing_date = ? AND scheduled_movie_start_time > ?;
+    ");
     
     mysqli_stmt_bind_param($statement, "i", $_GET["movie_id"]);
     read($statement);
@@ -22,12 +25,13 @@ switch ($_GET["action"]) {
     break;
     
   case "get-date-list":
-    $statement = mysqli_prepare($conn,
-                SELECT 
-                    Scheduled_Movie.ticket_payment_amount,
-                FROM Scheduled_Movie
-                JOIN Theater ON Scheduled_Movie.theater_id = Theater.theater_id
-                JOIN Cinema ON Theater.cinema_id = Cinema.cinema_id
+    $statement = mysqli_prepare($conn, "
+        SELECT 
+            Scheduled_Movie.scheduled_movie_showing_date,
+        FROM Scheduled_Movie
+        JOIN Theater ON Scheduled_Movie.theater_id = Theater.theater_id
+        JOIN Cinema ON Theater.cinema_id = Cinema.cinema_id
+        WHERE movie_id = ?, cinema_id = ?, scheduled_movie_showing_date >= ? 
     ");
     
     mysqli_stmt_bind_param($statement, "ii", $_GET["movie_id"], $_GET["cinema_id"]);
@@ -35,8 +39,13 @@ switch ($_GET["action"]) {
     break;
       
   case "get-time-list":
-    $statement = mysqli_prepare($conn,
-                  
+    $statement = mysqli_prepare($conn, "
+        SELECT 
+            Scheduled_Movie.scheduled_movie_starting_time,
+        FROM Scheduled_Movie
+        JOIN Theater ON Scheduled_Movie.theater_id = Theater.theater_id
+        JOIN Cinema ON Theater.cinema_id = Cinema.cinema_id
+        WHERE movie_id = ?, cinema_id = ?, scheduled_movie_showing_date >= ?, scheduled_movie_starting_time >= ?
     ");
     
     mysqli_stmt_bind_param($statement, "id", $_GET["movie_id"], $_GET["scheduled_movie_showing_date"]);
@@ -44,18 +53,23 @@ switch ($_GET["action"]) {
     break;
       
   case "get-scheduled-list":
-    $statement = mysqli_prepare($conn,
-                SELECT 
-                    
-                FROM Scheduled_Movie
-                JOIN 
-    ");                            
+    $statement = mysqli_prepare($conn, "
+        SELECT 
+            Scheduled_Movie.scheduled_movie_id
+        FROM Scheduled_Movie
+        JOIN Theater ON Scheduled_Movie.theater_id = Theater.theater_id
+        JOIN Cinema ON Theater.cinema_id = Cinema.cinema_id
+        WHERE movie_id = ?, cinema_id = ?, scheduled_movie_showing_date >= ?, scheduled_movie_starting_time >= ?
+    ");                           
     mysqli_stmt_bind_param($statement, "id", $_GET["movie_id"], $_GET["scheduled_movie_showing_date"], $_GET["scheduled_movie_showing_time"]);
     read($statement);
     break;
       
   case "get-seat":
-     $statement = mysqli_prepare($conn,
+    $statement = mysqli_prepare($conn, "
+        SELECT seat_id, ticket_id FROM Seat
+        WHERE scheduled_movie_id = ?
+        ORDER BY seat_row, seat_col ASC
      ");
     
      mysqli_stmt_bind_param($statement, "i", $_GET["scheduled_movie_id"]);
