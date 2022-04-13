889c49c207f226a826318028cbfab759097ce8c2
rayy1218 <raytham1218@gmail.com>
2022-04-13 10:57:36 +0800

    function to edit and delete scheduled-movie

--- src/script/schedule-employee.js
+++ src/script/schedule-employee.js
@@ -7,30 +7,21 @@ $(document).ready(() => {
                     action: "get-cinema-list",
                 },
                 success: (response) => {
-                    print(response);
+                    print(response)
                 },
-                error: () => {
-                    const result = [
-                        {cinema_id: 1, cinema_address: "Cinema First"},
-                        {cinema_id: 2, cinema_address: "Cinema Second"},
-                        {cinema_id: 3, cinema_address: "Cinema Third"}
-                    ];
-                    print(result);
-                }
             }
-        );
+        )
 
         function print(result) {
-            let append = "";
+            let append = ""
             for (let row of result) {
                 append += `<option value="${row.cinema_id}">${row.cinema_address}</option>`
             }
-            $("#schedule-cinema-select").append(append);
+            $("#schedule-cinema-select").append(append)
         }
     }
 
     function getScheduledMovie() {
-        console.log($("#schedule-cinema-select option:selected").val())
         $.ajax(
             "./api/schedule-employee.php",
             {
@@ -40,82 +31,19 @@ $(document).ready(() => {
                     "cinema-id": $("#schedule-cinema-select option:selected").val(),
                 },
                 success: (response) => {
-                    print(response);
-                },
-                error: () => {
-                    const random = Math.floor(Math.random() * (27 - 1) + 1);
-
-                    const result = [
-                        {
-                            scheduled_movie_id: 1,
-                            movie_title: "Movie 1",
-                            scheduled_movie_showing_date: random + "/12/2022",
-                            scheduled_movie_starting_time: "1200",
-                            movie_duration: 120,
-                        },
-                        {
-                            scheduled_movie_id: 2,
-                            movie_title: "Movie 2",
-                            scheduled_movie_showing_date: random + "/12/2022",
-                            scheduled_movie_starting_time: "1600",
-                            movie_duration: 120,
-                        },
-                        {
-                            scheduled_movie_id: 3,
-                            movie_title: "Movie 3",
-                            scheduled_movie_showing_date: random + "/12/2022",
-                            scheduled_movie_starting_time: "2000",
-                            movie_duration: 120,
-                        },
-                        {
-                            scheduled_movie_id: 4,
-                            movie_title: "Movie 4",
-                            scheduled_movie_showing_date: random + 1 + "/12/2022",
-                            scheduled_movie_starting_time: "1200",
-                            movie_duration: 120,
-                        },
-                        {
-                            scheduled_movie_id: 5,
-                            movie_title: "Movie 1",
-                            scheduled_movie_showing_date: random + 1 + "/12/2022",
-                            scheduled_movie_starting_time: "1600",
-                            movie_duration: 120,
-                        },
-                        {
-                            scheduled_movie_id: 6,
-                            movie_title: "Movie 1",
-                            scheduled_movie_showing_date: random + 2 + "/12/2022",
-                            scheduled_movie_starting_time: "0800",
-                            movie_duration: 120,
-                        },
-                        {
-                            scheduled_movie_id: 7,
-                            movie_title: "Movie 2",
-                            scheduled_movie_showing_date: random + 2 + "/12/2022",
-                            scheduled_movie_starting_time: "1200",
-                            movie_duration: 120,
-                        },
-                        {
-                            scheduled_movie_id: 8,
-                            movie_title: "Movie 3",
-                            scheduled_movie_showing_date: random + 2 + "/12/2022",
-                            scheduled_movie_starting_time: "1600",
-                            movie_duration: 120,
-                        },
-                    ];
-                    print(result);
+                    print(response)
                 }
             }
-        );
+        )
 
         function print(result) {
-            let append = "", date = "", accordion_index = 0;
+            let append = "", date = "", accordion_index = 0
             for (let i = 0; i < result.length; i += 1) {
-                const row = result[i];
+                const row = result[i]
                 
                 if (date !== row.scheduled_movie_showing_date) {
                     accordion_index += 1
-                    date = row.scheduled_movie_showing_date;
+                    date = row.scheduled_movie_showing_date
                     append += `
                     <div class="accordion-item">
                       <h2 class="accordion-header" id="heading${accordion_index}">
@@ -123,10 +51,10 @@ $(document).ready(() => {
                           ${row.scheduled_movie_showing_date}
                         </button>
                       </h2>
-                      <div id="collapse${accordion_index}" class="accordion-collapse collapse" aria-labelledby="heading${accordion_index}" data-bs-parent="#schedule-accordion">
+                      <div id="collapse${accordion_index}" class="accordion-collapse collapse" aria-labelledby="heading${accordion_index}" data-bs-parent="#scheduled-movie-placeholder">
                         <div class="accordion-body">
                           <ol class="list-group">
-                    `;
+                    `
                 }
                 
                 append += `
@@ -143,7 +71,7 @@ $(document).ready(() => {
                       </div>
                     </div>
                   </li>
-                `;
+                `
                 
                 if (i + 1 === result.length || date !== result[i + 1].scheduled_movie_showing_date) {
                     append += `
@@ -151,37 +79,189 @@ $(document).ready(() => {
                           </div>
                         </div>
                       </div>
-                    `;
+                    `
                 }
             }
 
-            $("#scheduled-movie-placeholder").html(append);
+            $("#scheduled-movie-placeholder").html(append)
         }
     }
 
-    getCinemaList();
+    function getScheduledMovieData() {
+        $.ajax(
+            "./api/schedule-employee.php",
+            {
+                type: "GET",
+                data: {
+                    action: "get-scheduled-movie-data",
+                    "scheduled-movie-id": $(".scheduled-movie-id-field").val()
+                },
+                success: (response1) => {
+                    let cinema_id = response1["cinema_id"], theater_id = response1["theater_id"]
 
-    $("#query-scheduled-movie-btn").attr("disabled", $("#schedule-cinema-select option:selected").val() === "not-selected");
+                    $.ajax("./api/cinema-theater.php",
+                        {
+                            type: "GET",
+                            data: {
+                                action: "get-cinema-list",
+                            },
+                            success: (response2) => {
+                                printCinemaList(response2, cinema_id)
+                            },
+                        }
+                    )
+
+                    $.ajax(
+                        "/src/api/schedule-employee.php",
+                        {
+                            data: {
+                                action: "get-theater-list",
+                                "scheduled-movie-id": $(".scheduled-movie-id-field").val()
+                            },
+                            success: (response3) => {
+                                printTheaterList(response3, theater_id)
+                            }
+                        }
+                    )
+
+                    printDatetime(response1)
+                }
+            }
+        )
+
+        function printCinemaList(response, id) {
+            let html = ""
+            for (let row of response) {
+                if (row["cinema_id"] === id) {
+                    html += `<option value="${row["cinema_id"]}" selected>${row["cinema_address"]}</option>`
+                }
+                else {
+                    html += `<option value="${row["cinema_id"]}">${row["cinema_address"]}</option>`
+                }
+            }
+            $("#schedule-cinema").append(html)
+        }
+
+        function printTheaterList(response, id) {
+            let append = "<option value=\"not-selected\">Select Theater</option>"
+            for (let row of response) {
+                if (row["theater_id"] === id) {
+                    append += `<option value="${row["theater_id"]}" selected>${row["theater_name"]}</option>`
+                }
+                else {
+                    append += `<option value="${row["theater_id"]}">${row["theater_name"]}</option>`
+                }
+            }
+            $("#schedule-theater").html(append)
+        }
+
+        function printDatetime(response) {
+            $("#schedule-date").val(response["scheduled_movie_showing_date"])
+            $("#schedule-time").val(response["scheduled_movie_start_time"])
+        }
+    }
+
+    function editScheduledMovie() {
+        $.ajax(
+            "./api/schedule-employee.php",
+            {
+                type: "POST",
+                data: {
+                    action: "edit-scheduled-movie",
+                    "scheduled-movie-id": $(".scheduled-movie-id-field").val(),
+                    "theater-id": $("#schedule-theater option:selected").val(),
+                    "scheduled-movie-showing-date": $("#schedule-date").val(),
+                    "scheduled-movie-start-time": $("#schedule-time").val()
+                },
+                success: () => {
+                    let edit_modal = new bootstrap.Modal(document.getElementById('edit-scheduled-movie-modal'))
+                    edit_modal.dispose()
+
+                    getScheduledMovie()
+                }
+            }
+        )
+    }
+
+    function deleteScheduledMovie() {
+        $.ajax(
+            "./api/schedule-employee.php",
+            {
+                type: "POST",
+                data: {
+                    action: "delete-scheduled-movie",
+                    "scheduled-movie-id": $(".scheduled-movie-id-field").val()
+                },
+                success: () => {
+                    let delete_modal = new bootstrap.Modal(document.getElementById('delete-scheduled-movie-modal'))
+                    delete_modal.dispose()
+
+                    getScheduledMovie()
+                }
+            }
+        )
+    }
+
+    getCinemaList()
+
+    $("#query-scheduled-movie-btn").attr("disabled", $("#schedule-cinema-select option:selected").val() === "not-selected")
     $("#schedule-cinema-select").change(() => {
-        $("#query-scheduled-movie-btn").attr("disabled", $("#schedule-cinema-select option:selected").val() === "not-selected");
-    });
+        $("#query-scheduled-movie-btn").attr("disabled", $("#schedule-cinema-select option:selected").val() === "not-selected")
+    })
     $("#query-scheduled-movie-btn").click(() => {
-        getScheduledMovie();
-    });
+        getScheduledMovie()
+    })
 
-    const modal = $("#delete-scheduled-movie-modal, #edit-scheduled-movie-modal");
-    modal.on("show.bs.modal", (event) => {
-        const scheduled_movie_id = event.relatedTarget.getAttribute('data-bs-scheduled-movie-id');
-        $(".scheduled-movie-id-field").val(scheduled_movie_id);
-    });
+
+    $("#delete-scheduled-movie-modal").on("show.bs.modal", (event) => {
+        const scheduled_movie_id = event.relatedTarget.getAttribute('data-bs-scheduled-movie-id')
+        $(".scheduled-movie-id-field").val(scheduled_movie_id)
+    })
+
+    $("#edit-scheduled-movie-modal").on("show.bs.modal", (event) => {
+        const scheduled_movie_id = event.relatedTarget.getAttribute('data-bs-scheduled-movie-id')
+        $(".scheduled-movie-id-field").val(scheduled_movie_id)
+        getScheduledMovieData()
+    })
+
+    $("#schedule-cinema").change(() => {
+        if ($("#schedule-cinema option:selected").val() !== "not-selected") {
+            $.ajax("./api/cinema-theater.php",
+                {
+                    type: "GET",
+                    data: {
+                        action: "get-theater-list",
+                        "cinema-id": $("#schedule-cinema option:selected").val()
+                    },
+                    success: (response) => {
+                        print(response)
+                    },
+                }
+            )
+
+            function print(result) {
+                console.log(result)
+                let html = "<option value=\"not-selected\" selected>Select Theater</option>"
+                for (let row of result) {
+                    html += `<option value="${row.theater_id}">${row.theater_name}</option>`
+                }
+                $("#schedule-theater").html(html)
+            }
+            $("#schedule-theater").attr("disabled", false)
+        }
+        else {
+            $("#schedule-theater").attr("disabled", true)
+        }
+
+    })
 
     $("#modal-edit-scheduled-movie-btn").click(() => {
-        window.location.reload();
-    });
+        editScheduledMovie()
+    })
 
     $("#modal-delete-scheduled-movie-btn").click(() => {
-        window.location.reload();
-    });
+        deleteScheduledMovie()
+    })
 
 
-});
+})
