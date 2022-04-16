3d738ca5566527af1f8303ff269ab4ff408f3d9f
rayy1218 <raytham1218@gmail.com>
2022-04-16 16:54:11 +0800

    to single quote

--- src/api/cinema-theater.php
+++ src/api/cinema-theater.php
@@ -11,41 +11,42 @@ else if (isset($_GET["action"])) {
 }
 
 switch ($action) {
-  case "get-cinema-list":
-    $statement = mysqli_prepare($conn,"SELECT cinema_id, cinema_address FROM Cinema");
-    read($statement);
-    break;
-                                
-  case "get-theater-list":
-    $statement = mysqli_prepare($conn,
-        "
+    case "get-cinema-list":
+        $statement = mysqli_prepare($conn, "SELECT cinema_id, cinema_address FROM Cinema");
+        read($statement);
+        break;
+
+    case "get-theater-list":
+        $statement = mysqli_prepare($conn,
+            "
             SELECT
             Theater.theater_id, Theater.theater_name
             FROM Theater
             WHERE cinema_id = ?
     ");
 
-    mysqli_stmt_bind_param($statement, "i", $_GET["cinema_id"]);
-    read($statement);
-    break;
-    
-  case "add-cinema": 
-    $statement = mysqli_prepare($conn,
-        "
+        mysqli_stmt_bind_param($statement, "i", $_GET["cinema_id"]);
+        read($statement);
+        break;
+
+    case "add-cinema":
+        $statement = mysqli_prepare($conn,
+            "
             INSERT INTO Cinema(cinema_address, cinema_status)
-            VALUES(?, "on")
+            VALUES(?, 'on')
     ");
     
     mysqli_stmt_bind_param($statement, "s", $_GET["cinema_address"]);
     set($statement);
     break;
-    
-  case "add-theater": 
-    $statement = mysqli_prepare($conn,
-        "
-        INSERT INTO Theater(theater_name, theater_status, cinema_id) VALUES (?, "on", ?)
+
+    case "add-theater":
+        $statement = mysqli_prepare($conn,
+            "
+        INSERT INTO Theater(theater_name, theater_status, cinema_id) VALUES (?, 'on', ?)
     ");
     
     mysqli_stmt_bind_param($statement, "si", $_GET["theater_name"], $_GET["cinema_id"]);
     set($statement);
     break;
+}
