539d311ec4bdd1d0d646a0e9e57a03a07db83806
kai054631 <88898686+kai054631@users.noreply.github.com>
2022-04-16 19:28:52 +0800

    Update homepage.php

--- src/api/homepage.php
+++ src/api/homepage.php
@@ -1,4 +1,6 @@
 <?php
+include "authentication.php";
+
 header("HTTP/1.1 501 Not Implemented");
 $action = 0;
 if (isset($_POST["action"])) {
@@ -11,12 +13,23 @@ switch ($action) {
 case "promoted-movie":
     //TBD
     break;
-case "recent-movie":
-    SELECT movie_thumbnail,movie_id,movie_title FROM Member
-	ORDER BY movie_added_time;
+	
+case "movie-today""recent-movie":
+    SELECT TOP 9 movie_thumbnail, movie_id, movie_title FROM Member
+	WHERE scheduled_movie_showing_date  = $date("Y/m/d");
     break;
+	
 case "movie-today":
-    SELECT movie_thumbnail, movie_id, movie_title FROM Member
-	ORDER BY  movie_showing_date = today;
+	$statement = mysqli_prepare($conn,"
+    SELECT movie_id,movie_title,movie_thumbnail FROM Scheduled_Movie
+	JOIN Movie USING(movie_id)
+	WHERE scheduled_movie_showing_date = ? LIMIT 9
+	");
+	date_default_timezone_set('Asia/Kuala_Lumur');
+	$date = date("y/m/d");
+	
+	mysqli_stmt_bind_param($statement,"s",$date);
+	read($statement);
     break;
+
 }
