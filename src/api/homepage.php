a2d04cfc9f093750090699a286271d941a6d6aec
rayy1218 <raytham1218@gmail.com>
2022-04-17 17:30:15 +0800

    indentation

--- src/api/homepage.php
+++ src/api/homepage.php
@@ -11,30 +11,25 @@ else if (isset($_GET["action"])) {
     $action = $_GET["action"];
 }
 switch ($action) {
-case "promoted-movie":
-    //TBD
-    break;
-	
-case "recent-movie":
-	$statement = mysqli_prepare($conn,"
-        SELECT movie_id, movie_title, movie_thumbnail FROM Movie
-        ORDER BY movie_id DESC LIMIT 9
-	");
+    case "recent-movie":
+        $statement = mysqli_prepare($conn,"
+            SELECT movie_id, movie_title, movie_thumbnail FROM Movie
+            ORDER BY movie_id DESC LIMIT 9
+        ");
 
-	read($statement);
-    break;
-	
-case "movie-today":
-	$statement = mysqli_prepare($conn,"
-        SELECT movie_id, movie_title, movie_thumbnail FROM Scheduled_Movie
-        JOIN Movie USING (movie_id)
-        WHERE scheduled_movie_showing_date = ? LIMIT 9
-	");
-	date_default_timezone_set('Asia/Kuala_Lumpur');
-	$date = date("y-m-d");
-	
-	mysqli_stmt_bind_param($statement,"s",$date);
-	read($statement);
-    break;
+        read($statement);
+        break;
 
+    case "movie-today":
+        $statement = mysqli_prepare($conn,"
+            SELECT movie_id, movie_title, movie_thumbnail FROM Scheduled_Movie
+            JOIN Movie USING (movie_id)
+            WHERE scheduled_movie_showing_date = ? LIMIT 9
+        ");
+        date_default_timezone_set('Asia/Kuala_Lumpur');
+        $date = date("y-m-d");
+
+        mysqli_stmt_bind_param($statement,"s",$date);
+        read($statement);
+        break;
 }
